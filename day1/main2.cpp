4ec36da5714f346d81b13ca1ac416fbe69cb4abe
Harini10614 <harinibairesh@gmail.com>
2026-05-26 16:12:36 +0530

    Add files via upload

---
+++ day1/main2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: Harini.B.S\n";
+    cout << "Dept: AIML\n";
+    cout << "Father Name: Shivaboraiah\n";
+    cout << "Mother Name: Kalavathi\n";
+    return 0;
+}
